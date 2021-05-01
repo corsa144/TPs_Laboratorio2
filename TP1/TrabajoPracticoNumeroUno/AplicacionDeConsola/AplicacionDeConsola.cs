dee834d5fa7d31def67024bd418e0fc40e670eeb
Cristian Deta <corsa144@gmail.com>
2021-05-01 19:47:24 -0300

    TP2 subido

---
+++ TP1/TrabajoPracticoNumeroUno/AplicacionDeConsola/AplicacionDeConsola.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+
+namespace AplicacionDeConsola
+{
+    class AplicacionDeConsola
+    {
+        static void Main(string[] args)
+        {
+            Numero num = new Numero();
+            Numero num2 = new Numero(13);
+            Console.WriteLine(num.BinarioDecimal("1100"));
+            Console.WriteLine("Este es el numero 2: {0}",num2.DecimalBinario(13));
+
+            Console.ReadKey();
+        }
+    }
+}
